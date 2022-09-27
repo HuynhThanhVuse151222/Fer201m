0c60c4bd8bdceeb2a2f4831a52b3238e22ccc11d
ToanNguyenThai <19522366@gm.uit.edu.vn>
2022-09-27 13:07:23 +0700

    fix: container color & modal

--- my-app/src/PlayerDetails.js
+++ my-app/src/PlayerDetails.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import { Players } from './shared/ListOfPlayers';
-import { useState } from 'react';
-const  PlayerDetails = (playerD) => {
-    return(
-        <div>
-        {Players.map((player)=>(
-            <div id='popup1' className='overlay'>
-            <div class="popup">
-            <img src = {player.img} alt='pic'/>
+import React from "react";
+import { Players } from "./shared/ListOfPlayers";
+import { useState } from "react";
+const PlayerDetails = (playerD) => {
+  return (
+    <div>
+      {Players.map((player) => (
+        <div id="popup1" className="overlay">
+          <div class="popup">
+            <img src={player.img} alt="pic" />
             <h2>{player.name}</h2>
-            <a className='close' href='#openPopUp'>&times;</a>
-            <div className='content'>
-              {player.info}
-            </div>
+            <a className="close" href="#openPopUp">
+              &times;
+            </a>
+            <div className="content">{player.info}</div>
           </div>
-          </div>
-              ))}
-          </div>
-    )
-        }
-        export default PlayerDetails;
-          
-            
-        
+        </div>
+      ))}
+    </div>
+  );
+};
+export default PlayerDetails;
